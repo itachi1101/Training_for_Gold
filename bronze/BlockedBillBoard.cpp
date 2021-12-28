ccb76f480f21db7dd9400a830b7ef9895a6aa324
Aviral <aviral1101@gmail.com>
2021-12-28 08:11:54 +0530

    new prob

--- bronze/BlockedBillBoard.cpp
+++ bronze/BlockedBillBoard.cpp
@@ -14,19 +14,31 @@ void fastIO() {ios_base::sync_with_stdio(false); cin.tie(NULL);}
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------
 string yes = "YES";
 string no = "NO";
+struct Rect {
+	int x1, y1, x2, y2;
+	int area() {
+		return (y2 - y1) * (x2 - x1);
+	}
+};
 //////////////////////////////////////////////JAI SHREE RAM /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int interset(Rect p, Rect q) {
+	int xOverlap = max(0, min(p.x2, q.x2) - max(p.x1, q.x1));
+	int yOverlap = max(0, min(p.y2, q.y2) - max(p.y1, q.y1));
+	return xOverlap * yOverlap;
+}
 void JaiShreeRam() {
-//write your code here
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	int intersection = max(min(b, d) - max(a, c), 0);
-	cout << ((b - a) + (d - c) ) - intersection;
+	Rect a, b, t;
+	cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
+	cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+	cin >> t.x1 >> t.y1 >> t.x2 >> t.y2;
+	cout << a.area() + b.area() - interset(a, t) - interset(b, t) << endl;
+
 }
 //////////////////////////////////////////////JAI SHREE RAM /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
 #ifndef ONLINE_JUDGE
-	freopen("paint.in", "r", stdin);
-	freopen("paint.out", "w", stdout);
+	freopen("billboard.in", "r", stdin);
+	freopen("billboard.out", "w", stdout);
 	freopen("error.txt", "w", stderr);
 #endif
 	fastIO();
